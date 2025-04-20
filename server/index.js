f3a2ac4b1a4cc9538e0171e0c6d50661afa211b2
MicsQuad <jegkorszak4@gmail.com>
2025-04-20 20:28:14 +0200

    Connected backend to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 const PORT = 5000;
@@ -7,9 +9,16 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Server is up and running!');
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('MongoDB connection error:', err);
 });
+
+app.get('/', (req, res) => res.send('Server is up!'));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
